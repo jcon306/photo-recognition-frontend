4a3184f5968848d5e0d8228d9fafbf3aab2aadf2
Joe C <consolinojoe@gmail.com>
2023-04-19 21:08:47 -0400

    changes to register

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -29,7 +29,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://smart-photo-backend.onrender.com/image', {
+        fetch('https://smart-photo-backend.onrender.com/register', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
